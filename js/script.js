411a1d48c17e8725a73bf5830a8eea3997c77045
Scarlett253 <yvonnarriola@gmail.com>
2026-02-11 21:38:03 -0500

    Added game variables and elements

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-// elements from the DOM
+/**Elements from the DOM*/
 let startBtn = document.getElementById("start-btn");
 let startScreen = document.getElementById("start-screen");
 let gameContainer = document.getElementById("game-container");
+let ui = document.getElementById("ui");
+
+/**Game elements*/
+let player = document.getElementById("avatar-player");
+let hidden = document.getElementById("avatar-hidden");
+let message = document.getElementById("message");
+let timerDisplay = document.getElementById("timer");
+let restartButton = document.getElementById("restart-button");
+
+/**Game variables*/
+let player1 = {
+  x: 100,
+  y: 100
+};
+
+let player2 = {
+  x: 500,
+  y: 300
+};
+let speed = 10;
+let timeLeft = 60;
+let timer;
+let gameOver = false;
 
 // when start button is clicked
 startBtn.addEventListener("click", function () {
@@ -10,4 +33,25 @@ startBtn.addEventListener("click", function () {
 
   // show game
   gameContainer.style.display = "block";
+
+  //ui display
+  ui.style.display = "block";
+
+  //start the game 
+  startGame();
+
+
 });
+
+/**Start game function */
+
+function startGame() {
+  updatePositions();
+  startTimer();
+}
+
+//positions setup
+function updatePositions() {
+
+}
+
