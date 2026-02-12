bdb20ecc9be5f570f8f1122cba718bc2a5b2661b
Scarlett253 <yvonnarriola@gmail.com>
2026-02-11 22:04:45 -0500

    added movement controls (p1 and p2)

--- js/script.js
+++ js/script.js
@@ -12,15 +12,17 @@ let timerDisplay = document.getElementById("timer");
 let restartButton = document.getElementById("restart-button");
 
 /**Game variables*/
-let player1 = {
+//Players
+let p1 = {
   x: 100,
   y: 100
 };
 
-let player2 = {
+let p2 = {
   x: 500,
   y: 300
 };
+
 let speed = 10;
 let timeLeft = 60;
 let timer;
@@ -40,7 +42,6 @@ startBtn.addEventListener("click", function () {
   //start the game 
   startGame();
 
-
 });
 
 /**Start game function */
@@ -52,6 +53,30 @@ function startGame() {
 
 //positions setup
 function updatePositions() {
-
+  //player1
+  player.style.left = p1.x + "px";
+  player.style.top = p1.y + "px";
+  //player2
+  hidden.style.left = p2.x + "px";
+  hidden.style.top = p2.y + "px";
 }
 
+/**Movement controls */
+document.addEventListener("keydown", function (e) {
+  if (gameover) return;
+
+  //Player 1 W A S D
+  if (e.key === "w" || e.key === "W") p1.y -= speed;
+  if (e.key === "s" || e.key === "S") p1.y += speed;
+  if (e.key === "a" || e.key === "A") p1.x -= speed;
+  if (e.key === "d" || e.key === "D") p1.x += speed;
+
+  //Player 2 arrows
+  if (e.key === "ArrowUp") p2.y -= speed;
+  if (e.key === "ArrowDown") p2.y += speed;
+  if (e.key === "ArrowLeft") p2.x -= speed;
+  if (e.key === "ArrowRight") p2.x += speed;
+
+  updatePositions();
+  checkDistance();
+});
